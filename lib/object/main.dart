a145d6933f069deae027c7f9fad7e52a30ce0c2b
Caleb <calebh101dev@icloud.com>
2025-09-27 22:31:09 -0500

    Page scaffold

--- lib/object/main.dart
+++ lib/object/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+enum ObjectType {
+  json,
+  yaml,
+  plist,
+}
+
 enum ObjectEditorTabType {
   base,
   json,
@@ -7,18 +13,6 @@ enum ObjectEditorTabType {
   plist,
   settings,
 }
-
-Widget objectEditorTabTypeToWidget(ObjectEditorTabType objectEditorTabType) {
-  switch (objectEditorTabType) {
-    case ObjectEditorTabType.base: return Text("Editor");
-    case ObjectEditorTabType.json: return Text("JSON");
-    case ObjectEditorTabType.yaml: return Text("YAML");
-    case ObjectEditorTabType.plist: return Text("PList");
-    case ObjectEditorTabType.settings: return Icon(Icons.settings);
-  }
-}
-
-Widget objectEditorTabTypeContent(ObjectEditorTabType input) {}
 
 class ObjectEditorPage extends StatefulWidget {
   final Map value;
@@ -83,5 +77,79 @@ class _ObjectEditorPageState extends State<ObjectEditorPage> {
         ]),
       ),
     );
+  }
+
+  Widget objectEditorTabTypeToWidget(ObjectEditorTabType objectEditorTabType) {
+    switch (objectEditorTabType) {
+      case ObjectEditorTabType.base: return Text("Editor");
+      case ObjectEditorTabType.json: return Text("JSON");
+      case ObjectEditorTabType.yaml: return Text("YAML");
+      case ObjectEditorTabType.plist: return Text("PList");
+      case ObjectEditorTabType.settings: return Icon(Icons.settings);
+    }
+  }
+
+  ObjectType? objectEditorTabTypeToObjectType(ObjectEditorTabType objectEditorTabType) {
+    switch (objectEditorTabType) {
+      case ObjectEditorTabType.json: return ObjectType.json;
+      case ObjectEditorTabType.yaml: return ObjectType.yaml;
+      case ObjectEditorTabType.plist: return ObjectType.plist;
+      default: return null;
+    }
+  }
+
+  Widget objectEditorTabTypeContent(ObjectEditorTabType objectEditorTabType) {
+    switch (objectEditorTabType) {
+      case ObjectEditorTabType.base: return ObjectEditor();
+      case ObjectEditorTabType.settings: return ObjectEditorSettings();
+
+      default: 
+        ObjectType? type = objectEditorTabTypeToObjectType(objectEditorTabType);
+        if (type == null) throw Exception("Invalid editor type: $objectEditorTabType");
+        return ObjectEditorPreview(type: type);
+    }
+  }
+}
+
+class ObjectEditor extends StatefulWidget {
+  const ObjectEditor({super.key});
+
+  @override
+  State<ObjectEditor> createState() => _ObjectEditorState();
+}
+
+class _ObjectEditorState extends State<ObjectEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ObjectEditorPreview extends StatefulWidget {
+  final ObjectType type;
+  const ObjectEditorPreview({super.key, required this.type});
+
+  @override
+  State<ObjectEditorPreview> createState() => _ObjectEditorPreviewState();
+}
+
+class _ObjectEditorPreviewState extends State<ObjectEditorPreview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ObjectEditorSettings extends StatefulWidget {
+  const ObjectEditorSettings({super.key});
+
+  @override
+  State<ObjectEditorSettings> createState() => _ObjectEditorSettingsState();
+}
+
+class _ObjectEditorSettingsState extends State<ObjectEditorSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
